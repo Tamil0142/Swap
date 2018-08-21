28290b7f0556033b83fabe007695dd707ba527fe
Tamil0142 <42166616+Tamil0142@users.noreply.github.com>
2018-08-21 23:05:44 +0530

    Create Swap.java

---
+++ Swap.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class wifi{
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		int b=in.nextInt();
+		int t=a;
+		a=b;
+		b=t;
+		System.out.println(a+" "+b);
+		
+}}
